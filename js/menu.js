1ed2d8649969778477517961d0c4751953cfb130
duongphong999 <duongphong999@gmail.com>
2022-09-23 15:06:51 +0700

    update navbar fixed

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,17 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 1000,
 	speedAsDuration: true
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        let otop= $('.section-3').offset().top - window.innerHeight;
+        if($(window).scrollTop()>otop){
+            $('.navbar').addClass('sticky');
+    
+        }else{
+            $('.navbar').removeClass('sticky');
+        }
+        
+    });
+
+});
