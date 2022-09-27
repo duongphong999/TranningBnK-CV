521e2cfcaf4874819fb81e7aa796e1e90e914272
duongphong999 <duongphong999@gmail.com>
2022-09-27 11:15:39 +0700

    update responsive

--- js/contactAPI.js
+++ js/contactAPI.js
@@ -1,21 +1,28 @@
 $(document).ready(function () {
-    $("form").submit(function (event) {
-      var formData = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        message: $("#message").val(),
-      };
-  
-      $.ajax({
-        type: "POST",
-        url: "https://632d5cf3519d17fb53bc70c7.mockapi.io/api/get_contact",
-        data: formData,
-        dataType: "json",
-        encode: true,
-      }).done(function (data) {
-        console.log(data);
-      });
-  
-      event.preventDefault();
+    $("#submit-btn").click(function (event) {
+        let name = $("#name").val()
+        let email = $("#email").val()
+        let subject = $("#subject").val()
+        let message =$("#message").val()
+        var formData = {
+            name:name,
+            email: email,
+            subject: subject,
+            message: message
+        };
+        console.log(name);
+
+        $.ajax({
+
+            type: "POST",
+            url: "https://633154713ea4956cfb5b3d40.mockapi.io/api/get_contact",// where you wanna post
+            data: formData,
+            dataType: "json",
+            error: function (jqXHR, textStatus, errorMessage) {
+                console.log(errorMessage); // Optional
+            },
+            success: function (data) { console.log(data) }
+
+        });
     });
-  });
+});
